801336ceba6f95b6e8eadccc87343d056c46bb54
isidzukuri <axesigon@gmail.com>
2016-09-12 10:21:44 +0300

    fix

--- chatbot.rb
+++ chatbot.rb
@@ -1,7 +1,7 @@
 require 'yaml'
 require 'sqlite3'
 require 'active_record'
-require 'awesome_print'
+# require 'awesome_print'
 
 ['lib/mixins/*.rb', 'lib/*.rb', 'lib/models/*.rb'].each do |path|
 	Dir[path].each {|file| require_relative file }
